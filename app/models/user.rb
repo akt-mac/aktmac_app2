8af6fb9fd420141f110a913a178128455fa338ea
akt-mac <akitamac+develop@gmail.com>
2020-05-14 15:12:25 +0900

    add User password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
 
+  CODE_VALUES = 0..999
+  validates :code, presence: true, inclusion: { in: CODE_VALUES }, uniqueness: true
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 4 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_041901) do
+ActiveRecord::Schema.define(version: 2020_05_14_060557) do
 
   create_table "users", force: :cascade do |t|
     t.integer "code", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_14_041901) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["code"], name: "index_users_on_code", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
