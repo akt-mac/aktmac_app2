d9bb1fe6f6271ddd1a746aff4ec83dd85aa5bb59
akt-mac <akitamac+develop@gmail.com>
2020-05-15 14:58:14 +0900

    add login logout function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+      flash[:success] = "ログインしました。"
+    else
+      flash[:danger] = "認証に失敗しました。"
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    log_out
+    flash[:success] = "ログアウトしました。"
+    redirect_to root_url
+  end
 end
