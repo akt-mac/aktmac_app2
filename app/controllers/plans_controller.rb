abbd2182e63d32531953cafddce02446efae0a54
akt-mac <akitamac+develop@gmail.com>
2020-05-23 11:36:33 +0900

    add plan edit page

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,29 @@
 class PlansController < ApplicationController
+  before_action :set_user_plans
+  before_action :set_plan, only: %i(edit)
+
   def new
   end
+
+  def edit
+  end
+
+  def update
+    flash[:info] = "テスト"
+    redirect_to user_url(current_user)
+  end
+
+  private
+
+  def set_user_plans
+    @user = User.find(params[:user_id])
+  end
+
+
+    def set_plan
+      unless @plan = @user.plans.find_by(id: params[:id])
+        flash[:danger] = "アクセス権限がありません。"
+        redirect_to @user
+      end
+    end
 end
